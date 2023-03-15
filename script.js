9942fba6ae778235598c1daa414e01a00f832539
Eriosvaldo Gomes <gomeseri774@gmail.com>
2023-03-15 10:23:23 -0300

    :tada: first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let form = document.getElementById("conectar");
+
+function conectar() {
+  form.style.display = "flex";
+}
+
+let a = document.getElementById("msg");
+
+function text() {
+  a.style.display = "flex";
+}
+function s() {
+  form.style.display = "none";
+}
